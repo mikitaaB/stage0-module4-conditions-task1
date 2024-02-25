16d99f8d0d75f20a9ab66828fc5c6b5f4f8230fd
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-25 03:48:06 +0300

    module 4. task 1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean cond = false;
+        if (cond) {
+            System.out.println(a);
+        } else {
+            System.out.println(a);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean cond = true;
+        if (cond) {
+            System.out.println(a);
+        } else {
+            System.out.println(a);
+        }
     }
 }
